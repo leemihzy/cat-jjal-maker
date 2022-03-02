f0a8ce0129cd902c40aaf1f2f9aa30dbf531fead
이민형 <leemihzy@gmail.com>
2022-03-03 03:30:40 +0900

    -made CRA

--- cat-jjal-maker-cra/src/App.js
+++ cat-jjal-maker-cra/src/App.js
@@ -58,7 +58,7 @@ const Form = ({ updateMainCat }) => {
         value={value}
         onChange={handleInputChange}
       />
-      <button type="submit">생성</button>
+      <button type="submit">생성2</button>
       <p style={{ color: "red" }}>{errorMessage}</p>
     </form>
   );
